180842dc19debf3b4d70604c6f7b8a2212cb18e0
Jacob Albert Jolman <jaap@jolman.eu>
2011-10-21 19:03:39 +0200

    Read and Learn
    Signed-off-by: Jacob Albert Jolman <jaap@jolman.eu>

--- language/english/store_lang.php
+++ language/english/store_lang.php
@@ -9,49 +9,66 @@
  */
 
 // Shortcuts
-$lang['store_list_stores']				= 'List Stores';
-$lang['store_add_store']				= 'Add Store';
+$lang['store_list_stores']					= 'List Stores';
+$lang['store_add_store']					= 'Add Store';
 
 // Flieds
-$lang['store_field_name']				= 'Store name';
+$lang['store_field_name']					= 'Store name';
+$lang['store_field_email']					= 'Store default email';
+$lang['store_field_email2']					= 'Additional emails (Separate with ",")';
+$lang['store_field_currency']				= 'Default currency';
+$lang['store_field_item_per_page']			= 'Items per Page';
+$lang['store_field_show_with_tax']			= 'Show with Tax';
+$lang['store_field_display_stock']			= 'Display Stock';
+$lang['store_field_allow_comments']			= 'Allow Comments';
+$lang['store_field_new_order_mail_alert']	= 'Mail Alert on new Ordner';
+$lang['store_field_active']					= 'Is active';
+$lang['store_field_is_default']				= 'Is default';
+$lang['store_field_agb']					= 'Store AGB';
+$lang['store_field_privacy_policy']			= 'Store Privacy Policy';
+$lang['store_field_delivery_information']	= 'Store Delivery Information';
+
+//Radios
+$lang['store_radio_yes']					= ' Yes ';
+$lang['store_radio_no']						= ' No ';
 
 // Label Admin Index
-$lang['store_new_store_label']          = 'New Store';
-$lang['store_edit_store_name']          = 'Edit store name';
-$lang['store_label']					= 'Store';
-$lang['store_num_categories_label']		= 'Categories';
-$lang['store_actions_label']			= 'Actions';
-$lang['store_upload_label']				= 'Upload';
-$lang['store_manage_label']				= 'Manage';
-$lang['store_delete_label']				= 'Delete';
-$lang['store_no_store_error']			= 'No Store created';
+$lang['store_new_store_label']         		= 'New Store';
+$lang['store_edit_store_name']          	= 'Edit store name';
+$lang['store_label']						= 'Store';
+$lang['store_num_categories_label']			= 'Categories';
+$lang['store_actions_label']				= 'Actions';
+$lang['store_upload_label']					= 'Upload';
+$lang['store_manage_label']					= 'Manage';
+$lang['store_delete_label']					= 'Delete';
+$lang['store_no_store_error']				= 'No Store created';
 
-$lang['store_name_label']				= 'Name';
-$lang['store_email_label']				= 'Email';
-$lang['store_allow_comments_label']		= 'Allow comments';
+$lang['store_name_label']					= 'Name';
+$lang['store_email_label']					= 'Email';
+$lang['store_allow_comments_label']			= 'Allow comments';
 
-$lang['store_active_label']				= 'Active';
-$lang['store_is_default_label']			= 'Default';
-$lang['store_edit_label']				= 'Edit';
-$lang['store_preview_label']			= 'Preview';
+$lang['store_active_label']					= 'Active';
+$lang['store_is_default_label']				= 'Default';
+$lang['store_edit_label']					= 'Edit';
+$lang['store_preview_label']				= 'Preview';
 
-$lang['store_add_category']				= 'Add category';
-$lang['store_add_product']				= 'Add product';
-$lang['store_general_options']			= 'General Options';
-$lang['store_email_additional_label']	= 'Additional Emails';
-$lang['store_currency_label']			= 'Currency';
-$lang['store_item_per_page_label']		= 'Items per Page';
-$lang['store_display_stock_label']		= 'Dispaly stock';
-$lang['store_statistics_label']			= 'Statistics';
-$lang['store_num_categories_label']		= '# Categories in Store';
-$lang['store_num_products_label']		= '# Products in Store';
-$lang['store_num_pending_orders']		= '# pending Orders';
-$lang['store_backup_data_label']		= 'Backup Data';
-$lang['store_restore_data_label']		= 'Restore Data';
-$lang['store_set_default_label']		= 'Set Default';
+$lang['store_add_category']					= 'Add category';
+$lang['store_add_product']					= 'Add product';
+$lang['store_general_options']				= 'General Options';
+$lang['store_email_additional_label']		= 'Additional Emails';
+$lang['store_currency_label']				= 'Currency';
+$lang['store_item_per_page_label']			= 'Items per Page';
+$lang['store_display_stock_label']			= 'Dispaly stock';
+$lang['store_statistics_label']				= 'Statistics';
+$lang['store_num_categories_label']			= '# Categories in Store';
+$lang['store_num_products_label']			= '# Products in Store';
+$lang['store_num_pending_orders']			= '# pending Orders';
+$lang['store_backup_data_label']			= 'Backup Data';
+$lang['store_restore_data_label']			= 'Restore Data';
+$lang['store_set_default_label']			= 'Set Default';
 
 //Create Store
 
-$lang['store_create_success']			= 'Store sucessfully created';
-$lang['store_content_label']			= 'Store Content';
-$lang['store_additional_info_label']	= 'Additional Information';
+$lang['store_create_success']				= 'Store sucessfully created';
+$lang['store_content_label']				= 'Store Content';
+$lang['store_additional_info_label']		= 'Additional Information';
