b8938fe4e5d5e58273a8d3b3d4b11c76d5c5d327
Kevin Meier <meier.kevin@gmail.com>
2011-10-26 17:35:14 +0200

    added basic add product
    Signed-off-by: Kevin Meier <meier.kevin@gmail.com>

---
+++ views/admin/add_product.php
@@ -0,0 +1,55 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * This is a store module for PyroCMS
+ *
+ * @author 		Jaap Jolman And Kevin Meier - pyrocms-store Team
+ * @website		http://jolman.eu
+ * @package 	PyroCMS
+ * @subpackage 	Store Module
+**/
+?>
+
+<?php if ($this->method == 'add_product'): ?>
+	<h3><?php echo lang('store_product_add_label');?></h3>
+<?php endif; ?>
+
+
+
+<?php echo form_open($this->uri->uri_string(), 'class="crud"'); ?>
+
+<div>
+	<ol>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_cat_add_name','name'); ?>
+			<?php echo form_input('name',set_value('name',''),'class="text" maxlength="50"'); ?>
+			<span class="required-icon tooltip"><?php echo lang('required_label');?></span>
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_cat_add_html','html'); ?>
+			<?php echo form_textarea('html',set_value('html',''),'class="wysiwyg-simple" maxlength="1000" rows="7"'); ?>
+			<span class="required-icon tooltip"><?php echo lang('required_label');?></span>
+		</li>
+		
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_cat_add_parent_id','parent_id'); ?>
+			<?php echo form_dropdown('parent_id',$this->store_m->make_categories_dropdown(),'class="text" maxlength="10"'); ?>
+			
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_cat_add_images_id','images_id'); ?>
+			<?php echo form_input('images_id',set_value('images_id',''),'class="text" maxlength="10"'); ?>
+			
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_cat_add_thumbnail','thumbnail_id'); ?>
+			<?php echo form_input('thumbnail_id',set_value('thumbnail_id',''),'class="text" maxlength="10"'); ?>
+			
+		</li>
+	</ol>
+	<?php echo form_hidden('config_id',set_value($this->uri->rsegment(3),$this->uri->rsegment(3)),'class="text" maxlength="10"'); ?>
+	<div class="buttons float-right padding-top">
+		<?php $this->load->view('admin/partials/buttons', array('buttons' => array('save', 'cancel') )); ?>
+	</div>
+</div>
+
+<?php echo form_close(); ?>
