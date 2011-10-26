11ff126c41716ca3933629bcc47ad2f2650ef805
Kevin Meier <meier.kevin@gmail.com>
2011-10-26 18:47:59 +0200

    edited add_products.php view
    Signed-off-by: Kevin Meier <meier.kevin@gmail.com>

--- controllers/admin.php
+++ controllers/admin.php
@@ -25,7 +25,7 @@ class Admin extends Admin_Controller
 				array('field' => 'name',					'label' => 'lang:store_field_name',					'rules' => 'trim|max_length[50]|required'),
 				array('field' => 'email',					'label' => 'lang:store_field_email',				'rules' => 'trim|max_length[100]|required|valid_email'),
 				array('field' => 'additional_emails',		'label' => 'lang:store_field_additional_emails',	'rules' => 'trim|max_length[100]|valid_emails'),
-				array('field' => 'currency',				'label' => 'lang:store_field_currency',				'rules' => 'trim|max_length[10]|required'),
+				array('field' => 'currency',				'label' => 'lang:store_field_currency',				'rules' => 'trim|max_length[10]|required|is_natural_no_zero'),
 				array('field' => 'item_per_page',			'label' => 'lang:store_field_item_per_page',		'rules' => 'trim|max_length[10]|required'),
 				array('field' => 'show_with_tax',			'label' => 'lang:store_field_show_with_tax',		'rules' => 'required'),
 				array('field' => 'display_stock',			'label' => 'lang:store_field_display_stock',		'rules' => 'required'),
@@ -163,24 +163,24 @@ class Admin extends Admin_Controller
 		$this->load->library('form_validation');
 		$this->form_validation->_field_data=array();
 		$fields = array(
-			array('field' => 'name',				'label' => 'store_product_add_name',				'rules' => 'trim|max_length[50]|required'),
-			array('field' => 'html',				'label' => 'store_product_add_html',				'rules' => 'trim|max_length[1000]|required'),
-			array('field' => 'categories_id',			'label' => 'store_product_add_categories_id',			'rules' => 'trim|max_length[10]|'),
-			array('field' => 'images_id',				'label' => 'store_product_add_images_id',			'rules' => 'trim|max_length[10]|'),
+			array('field' => 'name',					'label' => 'store_product_add_name',					'rules' => 'trim|max_length[50]|required'),
+			array('field' => 'html',					'label' => 'store_product_add_html',					'rules' => 'trim|max_length[1000]|required'),
+			array('field' => 'categories_id',			'label' => 'store_product_add_categories_id',			'rules' => 'trim|max_length[10]|required'),
+			array('field' => 'images_id',				'label' => 'store_product_add_images_id',				'rules' => 'trim|max_length[10]|'),
 			array('field' => 'thumbnail_id',			'label' => 'store_product_add_thumbnail_id',			'rules' => 'trim|max_length[10]|'),
 			array('field' => 'config_id',				'label' => 'store_product_add_store_config_id',			'rules' => 'trim|max_length[10]|'),
 			array('field' => 'products_id',				'label' => 'store_product_add_store_products_id',		'rules' => 'trim|max_length[10]|'),
 			array('field' => 'attributes_id',			'label' => 'store_product_add_store_attributes_id',		'rules' => 'trim|max_length[10]|'),
-			array('field' => 'meta_description',			'label' => 'store_product_add_meta_description',		'rules' => 'trim|max_length[1000]|'),
+			array('field' => 'meta_description',		'label' => 'store_product_add_meta_description',		'rules' => 'trim|max_length[1000]|'),
 			array('field' => 'meta_keywords',			'label' => 'store_product_add_meta_keywords',			'rules' => 'trim|max_length[1000]|'),
-			array('field' => 'price',				'label' => 'store_product_add_price',				'rules' => 'trim|max_length[10]|'),
-			array('field' => 'stock',				'label' => 'store_product_add_stock',				'rules' => 'trim|max_length[10]|'),
-			array('field' => 'limited',				'label' => 'store_product_add_limited',				'rules' => 'trim|max_length[10]|'),
+			array('field' => 'price',					'label' => 'store_product_add_price',					'rules' => 'trim|max_length[10]|required'),
+			array('field' => 'stock',					'label' => 'store_product_add_stock',					'rules' => 'trim|max_length[10]|'),
+			array('field' => 'limited',					'label' => 'store_product_add_limited',					'rules' => 'trim|max_length[10]|'),
 			array('field' => 'limited_used',			'label' => 'store_product_add_limited_used',			'rules' => 'trim|max_length[10]|'),
-			array('field' => 'discount',				'label' => 'store_product_add_discount',			'rules' => 'trim|max_length[10]|'),
-			array('field' => 'images_id',				'label' => 'store_product_add_images_id',			'rules' => 'trim|max_length[10]|'),
+			array('field' => 'discount',				'label' => 'store_product_add_discount',				'rules' => 'trim|max_length[10]|'),
+			array('field' => 'images_id',				'label' => 'store_product_add_images_id',				'rules' => 'trim|max_length[10]|'),
 			array('field' => 'thumbnail_id',			'label' => 'store_product_add_thumbnail_id',			'rules' => 'trim|max_length[10]|'),
-			array('field' => 'allow_comments',			'label' => 'store_product_add_allow_comments',			'rules' => 'trim|max_length[10]|')
+			array('field' => 'allow_comments',			'label' => 'store_product_add_allow_comments',			'rules' => 'trim|max_length[10]|required')
 			);
 
 		$this->form_validation->set_rules($fields);
--- views/admin/add_product.php
+++ views/admin/add_product.php
@@ -20,31 +20,67 @@
 <div>
 	<ol>
 		<li class="<?php echo alternator('even', ''); ?>">
-			<?php echo lang('store_cat_add_name','name'); ?>
+			<?php echo lang('store_product_add_category_id','categories_id'); ?>
+			<?php echo form_dropdown('categories_id',$this->store_m->make_categories_dropdown(),'class="text" maxlength="50"'); ?>
+			<span class="required-icon tooltip"><?php echo lang('required_label');?></span>
+		</li>	
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_name','name'); ?>
 			<?php echo form_input('name',set_value('name',''),'class="text" maxlength="50"'); ?>
 			<span class="required-icon tooltip"><?php echo lang('required_label');?></span>
 		</li>
 		<li class="<?php echo alternator('even', ''); ?>">
-			<?php echo lang('store_cat_add_html','html'); ?>
+			<?php echo lang('store_product_add_html','html'); ?>
 			<?php echo form_textarea('html',set_value('html',''),'class="wysiwyg-simple" maxlength="1000" rows="7"'); ?>
 			<span class="required-icon tooltip"><?php echo lang('required_label');?></span>
 		</li>
-		
 		<li class="<?php echo alternator('even', ''); ?>">
-			<?php echo lang('store_cat_add_parent_id','parent_id'); ?>
-			<?php echo form_dropdown('parent_id',$this->store_m->make_categories_dropdown(),'class="text" maxlength="10"'); ?>
-			
+			<?php echo lang('store_product_add_meta_description','meta_description'); ?>
+			<?php echo form_textarea('meta_description',set_value('meta_description',''),' maxlength="1000" rows="3"'); ?>
 		</li>
 		<li class="<?php echo alternator('even', ''); ?>">
-			<?php echo lang('store_cat_add_images_id','images_id'); ?>
+			<?php echo lang('store_product_add_meta_keywords','meta_keywords'); ?>
+			<?php echo form_input('meta_keywords',set_value('meta_keywords',''),'class="text" maxlength="50"'); ?>
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_attributes_id','attributes_id'); ?>
+			<?php echo form_input('attributes_id',set_value('attributes_id',''),'class="text" maxlength="50"'); ?>
+		</li>		
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_price','price'); ?>
+			<?php echo form_input('price',set_value('price',''),'class="text" maxlength="10"'); ?>
+			<span class="required-icon tooltip"><?php echo lang('required_label');?></span>
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_discount','discount'); ?>
+			<?php echo form_input('discount',set_value('discount',''),'class="text" maxlength="10"'); ?>
+		</li>	
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_stock','stock'); ?>
+			<?php echo form_input('stock',set_value('stock',''),'class="text" maxlength="10"'); ?>
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_limited','limited'); ?>
+			<?php echo form_input('limited',set_value('limited',''),'class="text" maxlength="10"'); ?>
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_limited_used','limited_used'); ?>
+			<?php echo form_input('limited_used',set_value('limited_used',''),'class="text" maxlength="10"'); ?>
+		</li>
+		<li class="<?php echo alternator('even', ''); ?>">
+			<?php echo lang('store_product_add_images_id','images_id'); ?>
 			<?php echo form_input('images_id',set_value('images_id',''),'class="text" maxlength="10"'); ?>
 			
-		</li>
+		</li>		
 		<li class="<?php echo alternator('even', ''); ?>">
-			<?php echo lang('store_cat_add_thumbnail','thumbnail_id'); ?>
-			<?php echo form_input('thumbnail_id',set_value('thumbnail_id',''),'class="text" maxlength="10"'); ?>
-			
+			<?php echo lang('store_product_add_thumbnail','thumbnail_id'); ?>
+			<?php echo form_input('thumbnail_id',set_value('thumbnail_id',''),'class="text" maxlength="10"'); ?>			
 		</li>
+			<li class="<?php echo alternator('even', ''); ?>">
+				<?php echo lang('store_product_add_allow_comments','allow_comments'); ?>
+                <?php echo form_radio('allow_comments','1',TRUE).$this->lang->line('store_radio_yes'); ?>
+                <?php echo form_radio('allow_comments','0',FALSE).$this->lang->line('store_radio_no'); ?>
+			</li>		
 	</ol>
 	<?php echo form_hidden('config_id',set_value($this->store_m->get_store_id(),$this->store_m->get_store_id()),'class="text" maxlength="10"'); ?>
 	<div class="buttons float-right padding-top">
